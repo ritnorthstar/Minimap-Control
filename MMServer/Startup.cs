dcb338f61587a8204e26bba9566fd66840b7260c
Josh Osgood <osgoodjw@gmail.com>
2013-12-02 11:57:48 -0500

    Add self-host MMServer project to Control solution
    - Duplicate of WebAPI self-hosted using OWIN

---
+++ MMServer/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace MMServer
+{
+    class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            // Return JSON by default
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            appBuilder.UseWebApi(config);
+        } 
+    }
+}
