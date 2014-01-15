ec5637b7bca0e4f0c9e5026dba63cdfe7959f388
Benjin Dubishar <benjin.dubishar@gmail.com>
2014-01-14 22:48:06 -0500

    Added internal line-drawing for table blocks

--- DataTypes/TableBlock.cs
+++ DataTypes/TableBlock.cs
@@ -27,12 +27,35 @@ namespace DataTypes
         public void DrawSelf(Canvas c, double zoomFactor)
         {
             Rectangle rect = new Rectangle();
-            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.Stroke = Brushes.Black;
             rect.Width = width * zoomFactor;
             rect.Height = height * zoomFactor;
             Canvas.SetLeft(rect, x1 * zoomFactor);
             Canvas.SetTop(rect, y1 * zoomFactor);
-            c.Children.Add(rect);
+
+            for (int n = 1; n < numTablesTall; n++)
+            {
+                Line l = new Line();
+                l.Stroke = Brushes.Black;
+                l.X1 = x1 * zoomFactor;
+                l.X2 = x2 * zoomFactor;
+                l.Y1 = (y1 + (height * (float)n / numTablesTall)) * zoomFactor;
+                l.Y2 = l.Y1;
+                c.Children.Add(l);
+            }
+
+            for (int n = 1; n < numTablesWide; n++)
+            {
+                Line l = new Line();
+                l.Stroke = Brushes.Black;
+                l.X1 = (x1 + (width * (float)n / numTablesWide)) * zoomFactor;
+                l.X2 = l.X1;
+                l.Y1 = y1 * zoomFactor;
+                l.Y2 = y2 * zoomFactor;
+                c.Children.Add(l);
+            }
+
+                c.Children.Add(rect);
         }
     }
 }
